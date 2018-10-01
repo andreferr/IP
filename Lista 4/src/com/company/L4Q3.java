0def2a51c85bd29daf5d2025b348ee40fe856244
André Ferreira <1680010+andreferr@users.noreply.github.com>
2018-10-01 16:50:17 -0300

    l4q3

--- Lista 4/src/com/company/L4Q3.java
+++ Lista 4/src/com/company/L4Q3.java
@@ -5,40 +5,60 @@ public class L4Q3 {
         Scanner in = new Scanner(System.in);
         int tamanho = Integer.parseInt(in.next()),jogadas = Integer.parseInt(in.next());
         int[][] matriz = new int[tamanho][tamanho];
-
+        boolean bomba = false;
         for (int i=0,j=0;j<matriz.length;j++,i=0) {
             for (;i<matriz.length;i++) {
                 matriz[j][i] = Integer.parseInt(in.next());
             }
         }
 
-        int[][] coordenadas = new int[2];
+        int[][] coordenadas = new int[jogadas][2];
 
         for (int k=0;k<jogadas;k++) {
             int[] soma = new int[3];
-            coordenadas[0] = Integer.parseInt(in.next());
-            coordenadas[1] = Integer.parseInt(in.next());
-            for (int l=0,b=0,d=0;j<matriz.length;j++,l=0,b=0,d=0) {
-                for (;coordenadas[0]+l<matriz.length;l++) {
-                    soma[0]+=coordenadas[0+i];
-                }
-                for (;coordenadas[1]+b<matriz.length;b++) {
-                    soma[1]+=coordenadas[1+p];
-                }
-                for (int x=0,y=0;coordenadas[1]+y<matriz.length;y++,x=0) {
-                    for (;coordenadas[0]+x<matriz.length;x++) {
-                        if (coordenadas[0+x] == coordenadas[1+y]) {
-                            soma[2] += coordenadas[0+x];
-                        }
+            coordenadas[k][0] = Integer.parseInt(in.next());
+            coordenadas[k][1] = Integer.parseInt(in.next());
+            if (matriz[coordenadas[k][0]][coordenadas[k][1]] == -1) {
+                System.out.println("Essa casa ja foi selecionada.");
+            } else {
+
+                for (int x = coordenadas[k][1], y = coordenadas[k][0]; x < matriz.length; x++) {
+                    if (matriz[y][x] == -1) {
+                    } else {
+                        soma[0] += matriz[y][x];
                     }
                 }
+                for (int x = coordenadas[k][1], y = coordenadas[k][0]; y < matriz.length; y++) {
+                    if (matriz[y][x] == -1) {
+                    } else {
+                        soma[1] += matriz[y][x];
+                    }
+                }
+                for (int x = coordenadas[k][1], y = coordenadas[k][0], q = 0, z = 0,p=0;(y+q) < matriz.length;q++,z=p) {
+                    for (;z<matriz.length-x;z++)
+                    if (matriz[y][x] == -1) {
+                    } else {
+                        soma[2] += matriz[y + q][x + z];
+                        break;
+                    }
+                    p++;
+                }
+                if (soma[0] == soma[1] && soma[1] == soma[2]) {
+                    System.out.println("Bomba encontrada.\nVoce perdeu.");
+                    bomba = true;
+                    break;
+                } else if (soma[0] != soma[1] || soma[0] != soma[2] || soma[1] != soma[2]) {
+                    System.out.println("Boa jogada, a casa ("+coordenadas[k][0]+","+coordenadas[k][1]+") nao tinha bomba.");
+                    matriz[coordenadas[k][1]][coordenadas[k][0]] = -1;
+                }
+
 
             }
         }
 
-
-
-
+        if (bomba==false) {
+            System.out.println("Voce e uma pessoa de sorte.");
+        }
 
 
     }
